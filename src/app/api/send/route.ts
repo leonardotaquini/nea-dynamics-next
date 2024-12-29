bba0f017a8965f28a4be0722fe2cb76c3bd01d26
leonardo taquini <leonardogtaquini@gmail.com>
2024-12-29 02:11:25 -0300

    Enhance email template styling and add error logging in API route

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -19,6 +19,7 @@ export async function POST(request: NextRequest) {
     });
 
     if (error) {
+      console.log(error.message);
       return Response.json({ error }, { status: 500 });
     }
 
--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -8,15 +8,20 @@ export interface EmailTemplateProps {
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ name, phone, email, message }) => (
-  <div>
-    <div>
-        <h1 className="text-blue-500 text-center">Hola mi nombre es {name}</h1>
-    </div>
-    <div className="leading-loose">
-        <p style={{color:'red'}}>{message}</p>
-    </div>
-    <footer className="text-blue text-center">
-        <p>Me pueden contactar a través de { email } o a mi número telefónico: {phone}</p>
-    </footer>
+  <div style={{ fontFamily: "Arial, sans-serif", color: "#333", lineHeight: "1.5", maxWidth: "600px", margin: "auto", border: "1px solid #ddd", padding: "20px", borderRadius: "8px", backgroundColor: "#f9f9f9" }}>
+  <div style={{ borderBottom: "2px solid #048CE2", marginBottom: "20px", paddingBottom: "10px" }}>
+    <h1 style={{ fontSize: "24px", color: "#003667", margin: "0" }}>
+      Hola, mi nombre es <span style={{ color: "#048CE2" }}>{name}</span>
+    </h1>
   </div>
+  <div>
+    <p style={{ fontSize: "16px", color: "#666", margin: "10px 0" }}>{message}</p>
+  </div>
+  <footer style={{ borderTop: "2px solid #048CE2", marginTop: "20px", paddingTop: "10px", textAlign: "center" }}>
+    <p style={{ fontSize: "14px", color: "#666", margin: "0" }}>
+      Me pueden contactar a través de <a href={`mailto:${email}`} style={{ color: "#048CE2", textDecoration: "none" }}>{email}</a> o a mi número telefónico: <a href={`tel:${phone}`} style={{ color: "#048CE2", textDecoration: "none" }}>{phone}</a>
+    </p>
+  </footer>
+</div>
+
 );
